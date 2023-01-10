eaf87d131aab139bdc4c1f72eaf647dba0ac5743
gurudattpuranik25 <gurudattpuranik35@gmail.com>
2023-01-10 23:14:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import registerImage from "./assets/register.jpg";
 
 function App() {
+  const [formData, setformData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const { name, email, password, confirmPassword } = formData;
+
+  const handleChange = (e) => {
+    setformData({ ...formData, [e.target.name]: [e.target.value] });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="registerSection">
+        <img src={registerImage} alt="" />
+        <div className="registerForm">
+          <h1>Devdock</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Confirm Password"
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={handleChange}
+              required
+            />
+            <div className="captcha">
+              <input type="text" id="captcha" placeholder="captcha" />
+              <i className="fa-solid fa-arrows-rotate"></i>
+            </div>
+            <button id="btnSubmit">Sign Up</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
